b146d9c7c5a1c15f0680d4fef62ca59278eadbba
MarinhoCM <marinhoc384@gmail.com>
2023-07-04 23:50:45 -0300

    :sparkles:feat: update file Iter.java with arrays exercices

--- maratona_java/src/academy/devdojo/maratonaJava/introducao/Aula07Arrays2Iter.java
+++ maratona_java/src/academy/devdojo/maratonaJava/introducao/Aula07Arrays2Iter.java
@@ -11,10 +11,11 @@ public class Aula07Arrays2Iter {
 //        System.out.println(ages[1]); // null;
 //        System.out.println(ages[2]); // null;
 //        System.out.println(ages[3]); // null;
-        String [] names = new String[3];
+        String [] names = new String[4];
         names[0] = "Goku";
         names[1] = "Vegeta";
         names[2] = "Bulma";
+        names[3] = "Kuririn";
         for (int i = 0; i <= 2; i++) {
             System.out.println(names[i]);
         }
