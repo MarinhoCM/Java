0796706ef2a9d053c3bb845f7aba071a0319989d
MarinhoCM <marinhoc384@gmail.com>
2023-07-04 23:20:59 -0300

    :sparkles:feat: Add arrays exercice

---
+++ maratona_java/src/academy/devdojo/maratonaJava/introducao/Aula07Arrays1.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonaJava.introducao;
+public class Aula07Arrays1 {
+    public static void main(String[] args) {
+        int age = 15;
+        int age2 = 20;
+        int age3 = 21;
+        // sintaxe = "tipo [] nome"
+        int [] ages = new int[3]; // ou int ages[];
+        // somente tipos de referencia podem ser iniciados como nulo;
+        // exemplo (String, Arrays);
+        System.out.println(ages[0]); // 0;
+        System.out.println(ages[1]); // 0;
+        System.out.println(ages[2]); // 0;
+//        System.out.println(ages[3]); // Exception: Index of bounds;
+        ages[0] = 21;
+        ages[1] = 15;
+        ages[2] = 11;
+//        ages[3] = 11; // Exception: Index of bounds;
+        System.out.println(ages[0]); // 21;
+        System.out.println(ages[1]); // 15;
+        System.out.println(ages[2]); // 11;
+    }
+}
