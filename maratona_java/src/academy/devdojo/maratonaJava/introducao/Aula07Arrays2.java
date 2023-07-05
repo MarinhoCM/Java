dfd7325418611799132ad1dc4bd406b425f001e1
MarinhoCM <marinhoc384@gmail.com>
2023-07-04 23:48:37 -0300

    :saparkles:feat: Add new files with Array exercices

---
+++ maratona_java/src/academy/devdojo/maratonaJava/introducao/Aula07Arrays2.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula07Arrays2 {
+    public static void main(String[] args) {
+        // byte, short, int, float e double = 0;
+        // char = '\u0000';
+        // boolean false;
+        // String null;
+        String [] ages = new String[4];
+        System.out.println(ages[0]); // null;
+        System.out.println(ages[1]); // null;
+        System.out.println(ages[2]); // null;
+        System.out.println(ages[3]); // null;
+    }
+}
---
+++ maratona_java/src/academy/devdojo/maratonaJava/introducao/Aula07Arrays2Iter.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula07Arrays2Iter {
+    public static void main(String[] args) {
+        // byte, short, int, float e double = 0;
+        // char = '\u0000';
+        // boolean false;
+        // String null;
+//        String [] ages = new String[4];
+//        System.out.println(ages[0]); // null;
+//        System.out.println(ages[1]); // null;
+//        System.out.println(ages[2]); // null;
+//        System.out.println(ages[3]); // null;
+        String [] names = new String[3];
+        names[0] = "Goku";
+        names[1] = "Vegeta";
+        names[2] = "Bulma";
+        for (int i = 0; i <= 2; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
