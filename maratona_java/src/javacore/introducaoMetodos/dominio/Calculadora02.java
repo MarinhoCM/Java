1e1f85a265ef01f44108780dee8c8c948e33d9ea
MarinhoCM <marinhoc384@gmail.com>
2023-07-06 23:41:53 -0300

    :sparkles:feat: add class Calculadora02.java

--- maratona_java/src/javacore/introducaoMetodos/dominio/Calculadora02.java
+++ maratona_java/src/javacore/introducaoMetodos/dominio/Calculadora02.java
@@ -1,2 +1,23 @@
-package javacore.introducaoMetodos.dominio;public class Calculadora02 {
+package javacore.introducaoMetodos.dominio;
+
+public class Calculadora02 {
+    public double soma(double num, double other_num){
+        return num + other_num;
+    }
+    public double subtrair(double num, double other_num) {
+        return num - other_num;
+    }
+    public double multiplicar(double num, double other_num){
+        return num * other_num;
+    }
+    public double dividir(double num, double other_num){
+        if (num < 1){
+            System.out.println("INSIRA UM NUMERO VALIDO!");
+        } else if (other_num < 1){
+            System.out.println("INSIRA UM NUMERO VALIDO!");
+        } else {
+            return num / other_num;
+        }
+        return 0.0;
+    }
 }
