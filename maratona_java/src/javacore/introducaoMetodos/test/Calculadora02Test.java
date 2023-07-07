6627ea9939d6709a9e61214dae0eb6c89a12cde3
MarinhoCM <marinhoc384@gmail.com>
2023-07-06 23:39:20 -0300

    :sparkles:feat: introducing a methods with returns

---
+++ maratona_java/src/javacore/introducaoMetodos/test/Calculadora02Test.java
@@ -0,0 +1,13 @@
+package javacore.introducaoMetodos.test;
+
+import javacore.introducaoMetodos.dominio.Calculadora;
+
+public class Calculadora02Test {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.soma(10, 10);
+        calculadora.subtrair(10, 9);
+        calculadora.multiplicar(10, 10);
+        calculadora.dividir(10, 5);
+    }
+}
