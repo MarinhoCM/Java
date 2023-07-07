eabfde8a132deddf0167a512635b98b3fa846301
MarinhoCM <marinhoc384@gmail.com>
2023-07-06 23:28:09 -0300

    :sparkles:feat: introducing a methods in java

--- maratona_java/src/javacore/introducaoMetodos/dominio/Calculadora.java
+++ maratona_java/src/javacore/introducaoMetodos/dominio/Calculadora.java
@@ -1,2 +1,22 @@
-package javacore.introducaoMetodos.dominio;public class Calculadora {
+package javacore.introducaoMetodos.dominio;
+
+public class Calculadora {
+    public void soma(int num, int num2){
+        System.out.println(num + num2);
+    }
+    public void subtrair(int num, int num2){
+        System.out.println(num - num2);
+    }
+    public void multiplicar(int num, int num2){
+        System.out.println(num * num2);
+    }
+    public void dividir(int num, int num2){
+        if (num < 1){
+            System.out.println("INSIRA UM VALOR VALIDO!");
+        } else if (num2 < 1){
+            System.out.println("INSIRA UM VALOR VALIDO!");
+        } else {
+            System.out.println(num / num2);
+        }
+    }
 }
